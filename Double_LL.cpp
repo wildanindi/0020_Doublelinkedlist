4b06277a87ea92a197572a5137dd9fc39b985ebc
wildanindi <wildanindi2@gmail.com>
2024-05-04 10:42:57 +0700

    menambahkan prosedur search yang berisi untuk mencari noMhs dan mengembalikan nilai true jika di temukan dan false jika tidak di temukan. juga previous akan menunjuk ke node sebelumnya dari node yang di temukan(jika ada)

--- Double_LL.cpp
+++ Double_LL.cpp
@@ -71,5 +71,12 @@ void addNode()
 
 bool search(int rollNo, Node **previous, Node **current)
 {
-    
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
 }

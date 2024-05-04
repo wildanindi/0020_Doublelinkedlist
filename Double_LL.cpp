5ac7efbe2153d7b8d38832fb31af0f12fb636e8e
wildanindi <wildanindi2@gmail.com>
2024-05-04 10:27:21 +0700

    menambahkan logika, jika list nya kosong maka buat node baru pada START

--- Double_LL.cpp
+++ Double_LL.cpp
@@ -28,5 +28,13 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
             return;
         }
+        // if the list is empty, make the new node the START
+        newNode-> next = START; // 3. make the new node point to the first node
+        if (START != NULL)
+        {
+            START->prev = newNode; // 4. make the first node point to the new node
+        }
+        newNode->prev = NULL;
+        START = newNode;
     }
 }
